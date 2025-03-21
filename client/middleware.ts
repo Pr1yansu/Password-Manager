abf56eda64f5b41c52c52fe0737b29b48e025e4f
Pr1yansu <priyansuchowdhury57@gmail.com>
2025-03-21 18:50:41 +0530

    Add environment configuration, update API base URL, and enhance login functionality with user device details

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,7 +1,20 @@
-import type {NextRequest} from 'next/server'
+import {NextRequest, NextResponse} from 'next/server'
+import {AUTH_ROUTES} from "@/routes";
+import {cookies} from 'next/headers'
 
-export function middleware(request: NextRequest) {
-    console.log('middleware', request.nextUrl.pathname)
+export async function middleware(request: NextRequest) {
+    const cookieStore = await cookies();
+    const cookie = cookieStore.get(process.env.COOKIE_KEY!);
+    const authRoute = AUTH_ROUTES.find(route => new RegExp(route).test(request.nextUrl.pathname));
+
+    console.log('authRoute', authRoute)
+    if (!cookie && !authRoute) {
+        return NextResponse.redirect(new URL("/login",request.url));
+    }
+    if (cookie && authRoute) {
+        return NextResponse.redirect(new URL("/",request.url));
+    }
+    return NextResponse.next();
 }
 
 export const config = {
